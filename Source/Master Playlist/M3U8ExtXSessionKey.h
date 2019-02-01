25173e7de56aeeae490d51dc417a0e9eb3befc78
Pierre <pierre@interactive-mobility.com>
2019-02-01 16:37:51 +0100

    .DS_Store banished!

---
+++ Source/Master Playlist/M3U8ExtXSessionKey.h
@@ -0,0 +1,17 @@
+//
+//  M3U8ExtXSessionKey.h
+//  M3U8Kit
+//
+//  Created by Pierre Perrin on 01/02/2019.
+//  Copyright © 2019 Allen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface M3U8ExtXSessionKey : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Source/Master Playlist/M3U8ExtXSessionKey.m
@@ -0,0 +1,13 @@
+//
+//  M3U8ExtXSessionKey.m
+//  M3U8Kit
+//
+//  Created by Pierre Perrin on 01/02/2019.
+//  Copyright © 2019 Allen. All rights reserved.
+//
+
+#import "M3U8ExtXSessionKey.h"
+
+@implementation M3U8ExtXSessionKey
+
+@end
