9fe3c2562140c7d1150a18e1e42521ba83092a9d
Frank <472730949@qq.com>
2020-10-22 15:18:39 +0800

    Remove redundant targets in project.

---
+++ M3U8Kit/M3U8Kit.h
@@ -0,0 +1,20 @@
+//
+//  M3U8Kit.h
+//  M3U8Kit
+//
+//  Created by Sun Jin on 2017/6/19.
+//  Copyright © 2017年 Allen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for M3U8KitDynamic.
+FOUNDATION_EXPORT double M3U8KitVersionNumber;
+
+//! Project version string for M3U8KitDynamic.
+FOUNDATION_EXPORT const unsigned char M3U8KitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <M3U8Kit/PublicHeader.h>
+
+#import "M3U8Parser.h"
+
